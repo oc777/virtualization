26fb2f3e8b444704f742ee41651f08d6709b5c3b
Olga Christensen <oc222ba@student.lnu.se>
2016-10-23 21:57:41 +0200

    Update dependencies.sh

--- vagrant/dependencies.sh
+++ vagrant/dependencies.sh
@@ -18,21 +18,21 @@ echo "installing NodeJS..."
 #nvm install node
 #nvm use node
 #curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash - #> /dev/null 2>&1
-sudo apt-get install -y nodejs #> /dev/null 2>&1
+sudo apt-get install -y nodejs > /dev/null 2>&1
 
 echo " "
 echo "---------------------------------------------------"
 
 echo "installing npm..."
-sudo apt-get install -y npm #> /dev/null 2>&1
+sudo apt-get install -y npm > /dev/null 2>&1
 
 echo " "
 echo "---------------------------------------------------"
 
 #Install RVM:
 echo "install rvm"
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 # > /dev/null 2>&1
-curl -sSL https://get.rvm.io | bash -s stable #> /dev/null 2>&1
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 > /dev/null 2>&1
+curl -sSL https://get.rvm.io | bash -s stable > /dev/null 2>&1
 #load RVM
 source ~/.rvm/scripts/rvm
 
@@ -41,9 +41,9 @@ echo "---------------------------------------------------"
 
 #install ruby
 echo "install ruby"
-rvm install $ruby_v #> /dev/null 2>&1
+rvm install $ruby_v > /dev/null 2>&1
 #set as default v
-rvm use $ruby_v --default #> /dev/null 2>&1
+rvm use $ruby_v --default > /dev/null 2>&1
 
 #install bundler & rails
 #gem install bundler rails
@@ -53,14 +53,14 @@ echo "---------------------------------------------------"
 
 #install Bundler
 echo "install bundler"
-gem install bundler --no-ri --no-rdoc #> /dev/null 2>&1
+gem install bundler --no-ri --no-rdoc > /dev/null 2>&1
 
 echo " "
 echo "---------------------------------------------------"
 
 #install rails
 echo "install rails"
-gem install rails -v 4.2.6 --no-ri --no-rdoc #> /dev/null 2>&1
+gem install rails -v 4.2.6 --no-ri --no-rdoc > /dev/null 2>&1
 
 
 echo " "
@@ -68,19 +68,11 @@ echo "---------------------------------------------------"
 
 #install Qt
 echo "install Qt"
-sudo apt-get -y install libqt4-dev libqtwebkit-dev
-
-#memcached
-#apt-get -y install php5-memcache memcached
-
+sudo apt-get -y install libqt4-dev libqtwebkit-dev > /dev/null 2>&1
 
 #echo "all installed"
 #touch /etc/dependencies-done
 
-#instal PostgreSQL
-#echo "setting up PostgreSQL"
-#sudo apt-get -y install postgresql postgresql-contrib
-#/vagrant/dbstart.sh
 
 echo " "
 echo "---------------------------------------------------"
