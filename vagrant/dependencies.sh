ee9d7ef4115d9facded09d7932512c283b25fa8e
Olga Christensen <oc222ba@student.lnu.se>
2016-10-23 20:38:08 +0200

    Update dependencies.sh

--- vagrant/dependencies.sh
+++ vagrant/dependencies.sh
@@ -8,7 +8,7 @@ echo " "
 echo "---------------------------------------------------"
 echo "install core dependencies"
 sudo apt-get update > /dev/null 2>&1
-sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev > /dev/null 2>&1
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libpq-dev > /dev/null 2>&1
 
 echo " "
 echo "---------------------------------------------------"
