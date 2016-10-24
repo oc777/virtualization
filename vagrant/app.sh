044ffd1b8e92a4fd5a28af7767eee836a4ba0b2b
Olga Christensen <oc222ba@student.lnu.se>
2016-10-24 14:23:33 +0200

    Update app.sh

--- vagrant/app.sh
+++ vagrant/app.sh
@@ -7,7 +7,16 @@ cd /vagrant
 git clone https://github.com/DefactoSoftware/Hours.git
 cd Hours
 
+#Bundle all the gems
+echo "Bundle all the gems"
+bundle install
+
+echo "run setup"
 ./bin/setup
+
+echo "db migrate"
+#rails db:migrate
+rake db:migrate
 
 echo "foreman start"
 foreman start
